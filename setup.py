89a2eeec05ce4938f6f9751d7adb5956dd3ed2c2
Gaurav Harsha <gauravharsha05@gmail.com>
2021-12-22 16:08:32 -0600

    link openmp library for extensions setup

--- setup.py
+++ setup.py
@@ -5,21 +5,24 @@ thermalcisd = Extension(
     'tfdccsd.ThermalCISD',
     sources=['tfdccsd/fort_src/ThermalCISD.f90', ],
     libraries=['lapack', 'blas'],
-    extra_f90_compile_args=["-O3", "-fopenmp"]
+    extra_f90_compile_args=["-O3", "-fopenmp"],
+    extra_link_args=["-lgomp"]
 )
 
 thermalccsd = Extension(
     'tfdccsd.ThermalCCSD',
     sources=['tfdccsd/fort_src/ThermalCCSD.f90', ],
     libraries=['lapack', 'blas'],
-    extra_f90_compile_args=["-O3", "-fopenmp"]
+    extra_f90_compile_args=["-O3", "-fopenmp"],
+    extra_link_args=["-lgomp"]
 )
 
 expvals = Extension(
     'tfdccsd.ExpVals',
     sources=['tfdccsd/fort_src/ExpVals.f90', ],
     libraries=['lapack', 'blas'],
-    extra_f90_compile_args=["-O3", "-fopenmp"]
+    extra_f90_compile_args=["-O3", "-fopenmp"],
+    extra_link_args=["-lgomp"]
 )
 
 setup(
